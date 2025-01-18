95e7c241c2d801789f4980cb0975da99cb531a7a
Brian Inca <brianincah10@gmail.com>
2025-01-18 10:05:44 -0500

    feat: minor changes

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -4,7 +4,7 @@ class Conexion
     private $host = "localhost";
     private $user = "root";
     private $password = "";
-    private $database = "papeleriaelangel";
+    private $database = "";
     private $conexion;
 
     public function conectar()
